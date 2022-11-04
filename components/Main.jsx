3916d3bfa89143fe110ecb452f8c8e3978f6a4d2
Sousa <w97950_a@sealedair.corp>
2022-11-04 16:45:28 -0300

    Main cover

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,15 +5,15 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 const Main = ({heading, message, message1}) => {
   return (
-    <div id='/' className="w-full h-screen text-left bg-auto bg-no-repeat bg-right bg-white sm:bg-opacity-50 sm:bg-[url('/assets/bground.svg')]">
+    <div id='/' className="w-full h-screen text-left bg-auto bg-no-repeat bg-right bg-white bg-50% bg-center bg-[url('/assets/bground.png')] ">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-start items-center">
         <div>
-            <p className='py-0 text-base sm:text-xl font-extrabold text-gray-700'>{heading}</p>
-            <h2 className='py-0 text-5xl sm:text-7xl font-black text-left text-transparent bg-clip-text bg-gradient-to-r from-[#770000] to-[#BD0000]'>{message}</h2>
-            <h2 className='py-0 text-5xl sm:text-7xl font-black text-left text-transparent bg-clip-text bg-gradient-to-r from-[#770000] to-[#BD0000]'>{message1}</h2>
-          </div>
+            <p className='py-0 text-base sm:text-xl font-extrabold text-gray-300 transition-opacity ease-in duration-700 opacity-90 hover:opacity-100'>{heading}</p>
+            <h2 className='py-0 text-5xl sm:text-7xl font-black text-left text-white transition-opacity ease-in duration-700 opacity-90 hover:opacity-100' >{message}</h2>
+            <h2 className='py-0 text-5xl sm:text-7xl font-black text-left  text-white transition-opacity ease-in duration-700 opacity-90 hover:opacity-100'>{message1}</h2>
         </div>
       </div>
+    </div>
     
   );
 };
