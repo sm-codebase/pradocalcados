55dc91bb9257bbe96c506a5fff3e00322292a52e
Willian Marinho <willian.m.sousa@gmail.com>
2022-11-15 12:55:21 -0300

    Main section2

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -13,7 +13,7 @@ import InstagramImg from './InstagramImg'
 const instagram = () => {
   return (
     <div id='social' className='w-full pt-20'>
-      <section className="pt-20 pb-48 bg-gradient-to-r from-[#770000] to-[#BD0000]">  
+        
       <div className="container mx-auto px-4">
             <div className="flex flex-wrap justify-center text-center mb-24">
               <div className="w-full lg:w-6/12 px-4">
@@ -28,7 +28,9 @@ const instagram = () => {
           </div>
         <div className="flex justify-center">
           <div className='h-48 w-96'>
-            <Carousel>
+            <Carousel
+            autoPlay
+            >
               <InstagramImg socialImg={IgImg1} />
               <InstagramImg socialImg={IgImg2} />
               <InstagramImg socialImg={IgImg3} />
