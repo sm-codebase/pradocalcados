90466115c1017d73b8aa3dd4a2ca9627e6422c54
willian.m.sousa@gmail.com <willian.m.sousa@gmail.com>
2023-07-15 11:58:11 -0300

    Fotos

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -1,50 +1,56 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import React from 'react'
-import IgImg1 from '../public/assets/social-media/ig-img-1.jpg'
-import IgImg2 from '../public/assets/social-media/ig-img-2.jpg'
-import IgImg3 from '../public/assets/social-media/ig-img-3.jpg'
-import IgImg4 from '../public/assets/social-media/ig-img-4.jpg'
-import IgImg5 from '../public/assets/social-media/ig-img-5.jpg'
-import IgImg6 from '../public/assets/social-media/ig-img-6.jpg'
-import InstagramImg from './InstagramImg'
-
+import { Carousel } from "react-responsive-carousel";
+import React from "react";
+import IgImg1 from "../public/assets/social-media/ig-img-1.jpg";
+import IgImg2 from "../public/assets/social-media/ig-img-2.jpg";
+import IgImg3 from "../public/assets/social-media/ig-img-3.jpg";
+import IgImg4 from "../public/assets/social-media/ig-img-4.jpg";
+import IgImg5 from "../public/assets/social-media/ig-img-5.jpg";
+import IgImg6 from "../public/assets/social-media/ig-img-6.jpg";
+import InstagramImg from "./InstagramImg";
 
 const instagram = () => {
   return (
-    <div id='social' className='w-full h-screen bg-gradient-to-r from-[#770000] to-[#BD0000]'>
-        
+    <div
+      id="social"
+      className="w-full h-screen bg-gradient-to-r from-[#770000] to-[#BD0000]"
+    >
       <div className="container mx-auto">
-            <div className="flex flex-wrap justify-center text-center">
-              <div className="w-full lg:w-6/12">
-                <p className='text-white font-thin mt-10'>conte com a gente</p>
-                <h2 className="text-white text-4xl font-bold">#useprado</h2>
-                <p className="text-white font-thin leading-relaxed m-4 text-blueGray-500">
-                Desenvolvemos parceiria com as melhores empresas da região de Campinas
-                </p>
-              </div>
-            </div>
-            
-          </div>
-        <div className="flex justify-center mt-10">
-          <div className='h-60 w-96 lg:[h-70]'>
-            <Carousel
-            autoPlay
-            infiniteLoop={true}
-            >
-              <InstagramImg socialImg={IgImg1} />
-              <InstagramImg socialImg={IgImg2} />
-              <InstagramImg socialImg={IgImg3} />
-              <InstagramImg socialImg={IgImg4} />
-              <InstagramImg socialImg={IgImg5} />
-              <InstagramImg socialImg={IgImg6} />
-            </Carousel>
-
+        <div className="flex flex-wrap justify-center text-center">
+          <div className="w-full lg:w-6/12">
+            <p className="text-white font-thin mt-10">conte com a gente</p>
+            <h2 className="text-white text-4xl font-bold">#useprado</h2>
+            <p className="text-white font-thin leading-relaxed m-4 text-blueGray-500">
+              Desenvolvemos parceiria com as melhores empresas da região de
+              Campinas
+            </p>
           </div>
         </div>
-     
+      </div>
+      <div className="flex justify-center mt-10">
+        <div className="h-60 w-96 lg:[h-70]">
+          <Carousel
+            autoPlay={true}
+            showArrows={false}
+            infiniteLoop={true}
+            interval={5000}
+            stopOnHover={false}
+            showThumbs={false}
+            showStatus={false}
+            showIndicators={false}
+            swipeable={false}
+          >
+            <InstagramImg socialImg={IgImg1} />
+            <InstagramImg socialImg={IgImg2} />
+            <InstagramImg socialImg={IgImg3} />
+            <InstagramImg socialImg={IgImg4} />
+            <InstagramImg socialImg={IgImg5} />
+            <InstagramImg socialImg={IgImg6} />
+          </Carousel>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default instagram
+export default instagram;
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,69 +1,64 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import React from 'react'
-import SlImgSm1 from '../public/assets/sl-img-sm-1.png'
-import SlImgSm2 from '../public/assets/sl-img-sm-2.png'
-import SlImgSm3 from '../public/assets/sl-img-sm-3.png'
-import SlImg1 from '../public/assets/sl-img-1.png'
-import SlImg2 from '../public/assets/sl-img-2.png'
-import SlImg3 from '../public/assets/sl-img-3.png'
-import SlImg4 from '../public/assets/sl-img-4.png'
+import { Carousel } from "react-responsive-carousel";
+import React from "react";
+import SlImgSm1 from "../public/assets/sl-img-sm-1.png";
+import SlImgSm2 from "../public/assets/sl-img-sm-2.png";
+import SlImgSm3 from "../public/assets/sl-img-sm-3.png";
+import SlImg1 from "../public/assets/sl-img-1.png";
+import SlImg2 from "../public/assets/sl-img-2.png";
+import SlImg3 from "../public/assets/sl-img-3.png";
+import SlImg4 from "../public/assets/sl-img-4.png";
 import MainSlider from "./MainSlider";
 import ProductsCarousel from "./ProductsCarousel";
 import Card from "./Card";
 
-
 const Main = () => {
   return (
     <div id="/" className="">
-    <div className='md:hidden'>
-      <div className='pt-10'>
-      <Carousel
-          autoPlay={true}
-          showArrows={false}
-          infiniteLoop={true}
-          interval={5000}
-          stopOnHover={false}
-          showThumbs={false}
-          showStatus={false}
-          showIndicators={false}
-          swipeable={false}
-        >
-              <MainSlider sliderImg={SlImgSm1} />  
-              <MainSlider sliderImg={SlImgSm2} />  
-              <MainSlider sliderImg={SlImgSm3} />  
-            </Carousel>  
+      <div className="md:hidden">
+        <div className="pt-10">
+          <Carousel
+            autoPlay={true}
+            showArrows={false}
+            infiniteLoop={true}
+            interval={5000}
+            stopOnHover={false}
+            showThumbs={false}
+            showStatus={false}
+            showIndicators={false}
+            swipeable={false}
+          >
+            <MainSlider sliderImg={SlImgSm1} />
+            <MainSlider sliderImg={SlImgSm2} />
+            <MainSlider sliderImg={SlImgSm3} />
+          </Carousel>
         </div>
-        
       </div>
-      <div className='hidden md:block sm:block'>
-        <div className='pt-10'>
-        <Carousel
-          autoPlay={true}
-          showArrows={false}
-          infiniteLoop={true}
-          interval={5000}
-          stopOnHover={false}
-          showThumbs={false}
-          showStatus={false}
-          showIndicators={false}
-          swipeable={false}
-        >
-                <MainSlider sliderImg={SlImg1} />
-                <MainSlider sliderImg={SlImg2} />
-                <MainSlider sliderImg={SlImg3} />
-                <MainSlider sliderImg={SlImg4} />
-              </Carousel>   
-          </div>  
+      <div className="hidden md:block sm:block">
+        <div className="pt-10">
+          <Carousel
+            autoPlay={true}
+            showArrows={false}
+            infiniteLoop={true}
+            interval={5000}
+            stopOnHover={false}
+            showThumbs={false}
+            showStatus={false}
+            showIndicators={false}
+            swipeable={false}
+          >
+            <MainSlider sliderImg={SlImg1} />
+            <MainSlider sliderImg={SlImg2} />
+            <MainSlider sliderImg={SlImg3} />
+            <MainSlider sliderImg={SlImg4} />
+          </Carousel>
+        </div>
       </div>
       <div className="mb-10">
-      <Card />
+        <Card />
       </div>
-                
     </div>
-      
-    
   );
 };
 
-export default Main
+export default Main;
